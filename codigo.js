fb36a7a79da933983c9966bf180201dd444b69c9
eduardo8991-teck <eduardo.8991@colegioprogresso.g12.br>
2026-02-13 00:29:08 -0300

    Update codigo.js

--- codigo.js
+++ codigo.js
@@ -1,26 +1,3 @@
-console.log("JS carregado");
-
-const supabaseUrl = "COLE_SUA_PROJECT_URL_AQUI";
-const supabaseKey = "COLE_SUA_PUBLISHABLE_KEY_AQUI";
-
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
-
-async function salvar() {
-  console.log("Botão clicado");
-
-  const nome = document.getElementById("nome").value;
-  const senha = document.getElementById("senha").value;
-
-  const { data, error } = await supabase
-    .from("usuários")
-    .insert([
-      { nome: nome, senha: senha }
-    ]);
-
-  if (error) {
-    console.log(error);
-    alert("Erro: " + error.message);
-  } else {
-    alert("Salvo com sucesso!");
-  }
+function teste() {
+    alert("JS está funcionando");
 }
